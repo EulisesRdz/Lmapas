95aae744e9621a5717b9e3f17e336e77cac14bda
eulisesrdz <echev7@gmail.com>
2015-05-28 17:30:05 -0500

    agregacion de comentarios

--- app/src/main/java/com/example/echeverria/lmapas/MainActivity.java
+++ app/src/main/java/com/example/echeverria/lmapas/MainActivity.java
@@ -22,7 +22,9 @@ public class MainActivity extends ActionBarActivity implements GoogleMap.OnMapCl
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Sirve para relacionar el mapa con el fragment.
         googlemap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
+        //indica que tipo de mapa se mostrar al iniciar
         googlemap.setMapType((GoogleMap.MAP_TYPE_NORMAL));
         googlemap.moveCamera(CameraUpdateFactory.newLatLngZoom(casa, 15));
 
@@ -39,25 +41,30 @@ public class MainActivity extends ActionBarActivity implements GoogleMap.OnMapCl
         //Mostrar / ocultar funcionalidad de Zoom
         googlemap.getUiSettings().setZoomGesturesEnabled(true);
 
+        //Indicamos un marcador en nuestro mapa
         googlemap.addMarker(new MarkerOptions().position(casa).title("Casa De Euli").snippet("Col. Invisur, Atenea #1").icon(BitmapDescriptorFactory.fromResource(android.R.drawable.ic_menu_compass)).anchor(0.5f, 0.5f));
         googlemap.setOnMapClickListener(this);
 
     }
 
+    //Metodo que sirve para mover la camara.
     public void moveCamera(View view) {
         CameraPosition cameraPosition = new CameraPosition.Builder().target(new LatLng(18.36138308979571, -100.68158861249685)).zoom(15).build();
         googlemap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
     }
 
+    //Metodo que sirve para realizar una animacion en la camara.
     public void animateCamera(View view) {
         if (googlemap.getMyLocation()!= null)
             googlemap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(googlemap.getMyLocation().getLatitude(),googlemap.getMyLocation().getLongitude()),15));
     }
 
+    //Metodo que sirve para añadir los marcadores utilizando un boton.
     public void addMarker(View view) {
         googlemap.addMarker(new MarkerOptions().position(new LatLng(googlemap.getCameraPosition().target.latitude, googlemap.getCameraPosition().target.longitude)));
     }
 
+    //Metodo que sirve para añadir los marcadores pulsando sobre el mapa.
     @Override
     public void onMapClick(LatLng puntoPulsado) {
         googlemap.addMarker(new MarkerOptions().position(puntoPulsado).icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_CYAN)));
@@ -70,9 +77,11 @@ public class MainActivity extends ActionBarActivity implements GoogleMap.OnMapCl
         return true;
     }
 
+    //Se crea un menu con diferentes opciones.
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
+        //Dependiendo de que opcion seleccione cambiara el tipo de mapa, como se muestra a continuacion.
         if (id == R.id.uno) {
             googlemap.setMapType((GoogleMap.MAP_TYPE_NORMAL));
             return true;
